32c8df04b243bd69f87b032644895dbdd2837406
Park <qseft523125@naver.com>
2020-12-22 13:59:34 +0900

    1222

--- js/list.js
+++ js/list.js
@@ -1,5 +1,27 @@
-document.write("<ul><li>Apple</li><li>Banana</li></ul>");
-let dan = 4;
-for (let i = 1; i < 10; i++) {
-    document.write('<ul style="list-style-type:circle ";><li>' + dan + '*' + i + '=<b>' +(dan * i) + '</b></li></ul>');
+
+function myFunc(){
+    document.write("<ul><li>Apple</li><li>Banana</li></ul>");
+    let dan = 4;
+    document.write('<ul>');
+    for (let i = 1; i <= 10; i++) {
+        if (i % 2 == 1) {
+            document.write('<li class="even">' + dan + '*' + i + '=<b>' + (dan * i) + '</b></li>');
+        } else{
+            document.write('<li class="odd">' + dan + '*' + i + '=<b>' + (dan * i) + '</b></li>');
+        }
+    }
+document.write('</ul>');
+}
+
+
+function myFunc2(){
+    <table>
+    let dan=4;
+    document.write('<tr>');
+    for(let i=1; i<=10; i++){
+        document.write('<td>'+dan+'</td>'+'<td>*</td>'+'<td>'+i+'</td>'+'<td>=</td>'+'<td>'+(dan*i)+'</td>');
+        
+    }
+    document.write('</tr>');
+    </table>
 }
